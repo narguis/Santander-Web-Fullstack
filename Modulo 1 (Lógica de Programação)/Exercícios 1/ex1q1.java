2047e29c45c6d4d796dd935a40046d7b4f417df5
Guilherme Narguis <107137104+narguis@users.noreply.github.com>
2022-12-02 01:03:50 -0300

    Create ex1q1.java

---
+++ Modulo 1 (Lógica de Programação)/Exercícios 1/ex1q1.java
@@ -0,0 +1,10 @@
+public class ex1q1 {
+    public static void main(String[] args){
+        int janeiro = 10_000;
+        int fevereiro = 17_000;
+        int marco = 23_000;
+
+        System.out.printf("O total de gastos foi %d", janeiro+fevereiro+marco);
+    }
+
+}
